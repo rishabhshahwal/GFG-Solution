eb41be43a37f0ab2bd1ab3c72bf8f5fb978c585d
Rishabh Kumar Shahwal <rishabhshahwal03@gmail.com>
2026-09-21 01:46:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Hollow Rectangle/print-hollow-rectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // Check if current position is on the boundary
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            // Move to the next line after completing a row
+            System.out.println();
+        }
+    }
+}
